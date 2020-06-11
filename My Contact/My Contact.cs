7ee03a8c75bf8a6acb847a298f07236cc2120672
Jan Hryniuk <jasiekhryniuk@gmail.com>
2020-06-11 17:10:14 +0000

    improvement: Added README.md file.

--- My Contact/My Contact.cs
+++ My Contact/My Contact.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Data;
 using System.Data.Entity.Validation;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using My_Contact.DAL;
 using My_Contact.Entity;
